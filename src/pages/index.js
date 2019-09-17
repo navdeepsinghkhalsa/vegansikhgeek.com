5f783cb74426cd79a0276535d8ade12264e9ad32
Navdeep Singh <vegansikhgeek@gmail.com>
2019-09-16 17:33:51 -0700

    Add default title

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ class BlogIndex extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
+        <SEO />
         <Bio />
         <article>
           <header>
